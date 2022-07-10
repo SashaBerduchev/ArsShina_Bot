3aecfce2fbbf404cd04fee25e156f276ba31c32e
Санёк Виничук <sashavinichuk@gmail.com>
2022-07-11 00:13:51 +0300

    ArsShina_Bot 1.0.0

--- ArsShina_Bot/Http/Post.cs
+++ ArsShina_Bot/Http/Post.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using Nancy.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ArsShina_Bot.Http
 {
@@ -17,7 +19,7 @@ namespace ArsShina_Bot.Http
 
         }
 
-        public static async void Send(string ctr, string act, string data=null)
+        public static async Task<string> Send(string ctr, string act, string data=null)
         {
             try
             {
@@ -26,9 +28,12 @@ namespace ArsShina_Bot.Http
                 if (data != null)
                 {
                     Trace.WriteLine("POST " + data);
-                    StringContent stringContent = new StringContent(data);
-                    HttpResponseMessage request = await client.PostAsync(constr, stringContent);
-                    Trace.WriteLine(request);
+                    var poststr = data.Trim('/');
+                    StringContent stringContent = new StringContent(poststr);
+                    HttpResponseMessage request = await client.PostAsync(constr, new StringContent(new JavaScriptSerializer().Serialize(data), Encoding.UTF8, "application/json"));
+                    var ddd = request.Content.ReadAsStringAsync().Result;
+                    Trace.WriteLine(ddd);
+                    return request.Content.ReadAsStringAsync().Result;
                 }
                 else
                 {
@@ -37,14 +42,15 @@ namespace ArsShina_Bot.Http
                     Trace.WriteLine("POST  " + response);
                     string responseBody = await response.Content.ReadAsStringAsync();
                     Trace.WriteLine("RESPONSE" + responseBody.ToString());
-                    //ParsData(ctr, window, responseBody);
+                    return responseBody;
                 }
+                return "";
 
             }
             catch (Exception e)
             {
                 Trace.WriteLine(e.ToString());
-                //MessageBox.Show("Не удается подключится к серверу");
+                return "Не удается подключится к серверу";
             }
         }
     }
--- ArsShina_Bot/Program.cs
+++ ArsShina_Bot/Program.cs
@@ -13,7 +13,8 @@ namespace ArsShina_Bot
 {
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("5578780345:AAHU8lkJPlRgtOc13xYoDnVtzc_Nc1k5HDE");
+        
+        static ITelegramBotClient bot = new TelegramBotClient("5520950526:AAFnQh_ejXv-SztIkbnTihawdwARWhwJ_Lg");
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
@@ -35,15 +36,45 @@ namespace ArsShina_Bot
                 var message = update.Message;
                 if (message.Text.ToLower() == "/start")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+                    await botClient.SendTextMessageAsync(message.Chat, "Привет " + update.Message.From.FirstName);
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на сайт АвтоРесурс Сервис!");
+                    await botClient.SendTextMessageAsync(message.Chat, "Хотите посмотреть каталог брендов?");
                     Http.User user = new Http.User(update.Message.From.FirstName, update.Message.From.LastName);
                     var senddata = JsonConvert.SerializeObject(user);
-                    Post.Send("Users", "SaveTelegramUser", senddata);
+                    string str = Post.Send("Users", "SaveTelegramUser", senddata).Result;
                     return;
                 }
                 if (message.Text.ToLower() == "ги")
                 {
                     await botClient.SendTextMessageAsync(message.Chat, "Луцк привет!!");
+                }
+                if (message.Text.ToLower() == "да" || message.Text.ToLower() == "так")
+                {
+                    string str = Post.Send("Tires", "GetBotTires").Result;
+                    if (str != "")
+                    {
+                        string[] brends = JsonConvert.DeserializeObject<string[]>(str);
+                        for (int i = 0; i < brends.Length; i++)
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat, "/" + brends[i]);
+                        }
+
+                        await botClient.SendTextMessageAsync(message.Chat, "Выберите бренд");
+                    }
+
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Виберите ширину");
+                    string strwidth = Post.Send("Tires", "GetBotWidth", message.Text).Result;
+                    if (strwidth != "")
+                    {
+                        string[] width = JsonConvert.DeserializeObject<string[]>(strwidth);
+                        for (int i = 0; i < width.Length; i++)
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat, "/" + width[i]);
+                        }
+                    }
                 }
             }
         }
